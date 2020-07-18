e5c1fb94f005e1fb85d61589eb427038efd55780
Vangeepuram Sreekesari <kesari.vangeepuram@gmail.com>
2020-07-18 14:20:52 +0530

    All the Python Scripts were styled, obeying PEP8 Guide.

--- rot13_cipher.py
+++ rot13_cipher.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python3
+"""This python script rotates the ASCII text in the submitted file
+with ROT13 Algorithm, either to encrypt or to decrypt the text."""
 
-from rot13_dict import *
-import sys
 import os
+import sys
+from rot13_dict import rot_dict
 
 def rot_file(filename, encrypted_file):
-	with open(filename) as file:
-
-		with open(encrypted_file, "w") as cipher_file:
-			read = file.read()
-
-			for line in read:
-				for letter in line:
-					if letter.isalpha():
-						cipher_file.write(rot_dict[letter])
-					else:
-						cipher_file.write(letter)
+    "Returns the decrypted/encrypted file using the dictionary for rotating the letters."
+    with open(filename) as file:
+        with open(encrypted_file, "w") as cipher_file:
+            read = file.read()
+            for line in read:
+                for letter in line:
+                    if letter.isalpha():
+                        cipher_file.write(rot_dict[letter])
+                    else:
+                        cipher_file.write(letter)
 
 source = sys.argv[1]
 destination = sys.argv[2]
 
 if os.path.isfile(source):
 
-	if '~' in source:
-		source = os.path.expanduser('~') + source[1:]
-	elif '~' in destination:
-		destination = os.path.expanduser('~') + destination[1:]
-	else:
-		pass
+    if '~' in source:
+        source = os.path.expanduser('~') + source[1:]
+    elif '~' in destination:
+        destination = os.path.expanduser('~') + destination[1:]
+    else:
+        pass
 
 elif os.path.isdir(source):
-	print(f"{source} is a directory.")
-	print("Submit a file in ASCII text format for its encryption.")
-	sys.exit(1)
+    print(f"{source} is a directory.")
+    print("Submit a file in ASCII text format for its encryption.")
+    sys.exit(1)
 
 else:
-	print(f"{source} doesn't exist.")
-	print("Please submit a file existing in your computer!")
-	sys.exit(1)
+    print(f"{source} doesn't exist.")
+    print("Please submit a file existing in your computer!")
+    sys.exit(1)
 
 #Encrypting the plaintext file with ROT13 Subtitution Cipher:
 rot_file(source, destination)
--- rot13_dict.py
+++ rot13_dict.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python3
-'''
-Capital Letters:
-A B C D E F G H I J K L M
-N O P Q R S T U V W X Y Z
-'''
+"This python script generates a dictionary for rotation of ASCII text."
 
-'''
-Small Letters:
-a b c d e f g h i j k l m
-n o p q r s t u v w x y z
-'''
-
-rot_dict_phase_1 ={
-'A':'N',
-'B':'O',
-'C':'P',
-'D':'Q',
-'E':'R',
-'F':'S',
-'G':'T',
-'H':'U',
-'I':'V',
-'J':'W',
-'K':'X',
-'L':'Y',
-'M':'Z'
+rot_dict_phase_1 = {
+    'A':'N',
+    'B':'O',
+    'C':'P',
+    'D':'Q',
+    'E':'R',
+    'F':'S',
+    'G':'T',
+    'H':'U',
+    'I':'V',
+    'J':'W',
+    'K':'X',
+    'L':'Y',
+    'M':'Z'
 }
-rot_dict_phase_2 = {k : v for v,k in rot_dict_phase_1.items()}
+rot_dict_phase_2 = {k : v for v, k in rot_dict_phase_1.items()}
 
 cap_rot_dict = {**rot_dict_phase_1, **rot_dict_phase_2}
-small_rot_dict = {k.lower() : v.lower() for k,v in cap_rot_dict.items()}
+small_rot_dict = {k.lower() : v.lower() for k, v in cap_rot_dict.items()}
 
 rot_dict = {**cap_rot_dict, **small_rot_dict}
-
