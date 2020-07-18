c864e286cf56312ee9ac1d825404fe34d2bcb929
Vangeepuram Sreekesari <kesari.vangeepuram@gmail.com>
2020-07-18 14:57:14 +0530

    Improved the code, for concise and elegent look.

--- rot13_dict.py
+++ rot13_dict.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python3
 "This python script generates a dictionary for rotation of ASCII text."
 
-rot_dict_phase_1 = {
-    'A':'N',
-    'B':'O',
-    'C':'P',
-    'D':'Q',
-    'E':'R',
-    'F':'S',
-    'G':'T',
-    'H':'U',
-    'I':'V',
-    'J':'W',
-    'K':'X',
-    'L':'Y',
-    'M':'Z'
-}
+rot_dict_phase_1 = {chr(letter) : chr(letter+13) for letter in range(65, 78)}
 rot_dict_phase_2 = {k : v for v, k in rot_dict_phase_1.items()}
 
 cap_rot_dict = {**rot_dict_phase_1, **rot_dict_phase_2}
